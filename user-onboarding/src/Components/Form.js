32d5e36f28f05efdf41da127cde8d2e141307cdc
Arbor Matos <arbor@MacBook-Pro.local>
2022-07-28 11:30:26 -0700

    Building out the first form input.

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,4 +1,35 @@
-export default function Form(){
+import React from 'react'
+
+export default function Form(props){
+    const {
+        values
+    } = props;
+
+    const onSubmit(){
+        return;
+    }
+    const onChange(){
+        return;
+    }
+
+
+
+
+    return (
+        <div className="onboardingForm">
+            <form>
+
+                <label>Hello, My Name Is ___</label><br/>
+                <input
+                    name="name"
+                    type="text"
+                    value={values.name}
+                    onchange={onChange}
+                />
+
+            </form>
+        </div>
+    )
     // - [ ] Name (first_name, last_name)
     // - [ ] Email
     // - [ ] Password
