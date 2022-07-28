dfa5d9284695ec1ef79d8466ffd7560923fcbdd3
Arbor Matos <arbor@MacBook-Pro.local>
2022-07-28 10:15:55 -0700

    Initial commit, repo set up

---
+++ user-onboarding/src/Components/App.js
@@ -0,0 +1,20 @@
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          FIGHT FOR ME IN THE MODERN DAY
+        </p>
+      </header>
+      <div className="App-body">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,7 @@
+export default function Form(){
+    // - [ ] Name (first_name, last_name)
+    // - [ ] Email
+    // - [ ] Password
+    // - [ ] Terms of Service (checkbox)
+    // - [ ] A Submit button to send our form data to the server.
+}
